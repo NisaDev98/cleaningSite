0e63b9ea6625d5ac094503c486db1c9a87e23d3c
NisaDev98 <nisa98dev@gmail.com>
2023-04-05 11:47:42 +0200

    New Deployed Website

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,13 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react'
 import './navbar.css';
 const Navbar = () => {
-
-    const navRef = useRef();
-
-	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
-	};
-
+const [navIcon, setNavIcon]=useState(true)
+function navToggle(){
+    setNavIcon(prevState => !prevState)
+}
 
   return (
     <nav className="Navbar">
@@ -20,20 +17,16 @@ const Navbar = () => {
                     <img className='logoImg' src="/images/logo192.png" alt="Logo"  />
                 </div>
             </div>
-            <div className="navRight" ref={navRef}>
+            <div className='nav-btn' onClick={navToggle}>
+                <FontAwesomeIcon icon={navIcon ? faBars : faXmark} ></FontAwesomeIcon>
+            </div>
+            <div className="navRight" >
                 <a href='#' className='navMenu'>Home</a>
                 <a href='#' className='navMenu'>About</a>
                 <a href='#' className='navMenu'>Services</a>
                 <a href='#' className='navMenu'>FAQ</a>
                 <a href='#' className='navMenu'>Contact Us</a>
-                <button>
-                    <FontAwesomeIcon className="nav-btn nav-close-btn"
-					onClick={showNavbar} icon={faXmark}></FontAwesomeIcon>
-                </button>
             </div>
-            <button>
-                <FontAwesomeIcon className="nav-btn" onClick={showNavbar} icon={faBars}></FontAwesomeIcon>
-            </button>
         </div>
     </nav>
   )
